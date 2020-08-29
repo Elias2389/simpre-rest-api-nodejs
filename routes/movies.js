a9d7877bfb336d1fcfea5821ed64110745ac2c0c
Andres <andres.rivas@mercadolibre.com>
2020-08-29 10:17:06 -0300

    validation with schema

--- routes/movies.js
+++ routes/movies.js
@@ -1,80 +1,101 @@
 const express = require('express');
 const MoviesServices = require('../service/movies');
+const {
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema,
+} = require('../utils/schemas/movies');
+const validationHandler = require('../utils/middleware/validationHandler');
 
 const moviesService = new MoviesServices();
 
 function moviesApi(app) {
-    const router = express.Router();
+  const router = express.Router();
 
-    app.use("/api/movies", router);
+  app.use('/api/movies', router);
 
-    router.get("/", async (req, res, next) => {
-        const { tags } = req.query;
-        try {
-           const movies = await moviesService.getMovies({ tags });
-           res.status(200).json({
-               data: movies,
-               message: 'Movies listed'
-           });
-        } catch(err) {
-            next(err)
-        }
-    });
+  router.get('/', async (req, res, next) => {
+    const { tags } = req.query;
+    try {
+      const movies = await moviesService.getMovies({ tags });
+      res.status(200).json({
+        data: movies,
+        message: 'Movies listed',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 
-    router.get("/:movieId", async (req, res, next) => {
-        const { movieId } = req.params;
-        try {
-           const movies = await moviesService.getMovie({ movieId });
-           res.status(200).json({
-               data: movies,
-               message: 'Movies retrieve'
-           });
-        } catch(err) {
-            next(err)
-        }
-    });
+  router.get(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    async (req, res, next) => {
+      const { movieId } = req.params;
+      try {
+        const movies = await moviesService.getMovie({ movieId });
+        res.status(200).json({
+          data: movies,
+          message: 'Movies retrieve',
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
-    router.post("/", async (req, res, next) => {
-        const { body: movie } = req;
-        try {
-           const createMovieId = await moviesService.createMovie({ movie });
-           res.status(201).json({
-               data: createMovieId,
-               message: 'Movie created'
-           });
-        } catch(err) {
-            next(err)
-        }
-    });
+  router.post(
+    '/',
+    validationHandler(createMovieSchema),
+    async (req, res, next) => {
+      const { body: movie } = req;
+      try {
+        const createMovieId = await moviesService.createMovie({ movie });
+        res.status(201).json({
+          data: createMovieId,
+          message: 'Movie created',
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
-    router.put("/:movieId", async (req, res, next) => {
-        const { body: movie } = req;
-        const { movieId } = req.params;
-        try {
-           const updateMovie = await moviesService.updateMovie({ movieId, movie });
-           res.status(200).json({
-               data: updateMovie,
-               message: 'Movie updated'
-           });
-        } catch(err) {
-            next(err)
-        }
-    });
+  router.put(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    validationHandler(updateMovieSchema),
+    async (req, res, next) => {
+      const { body: movie } = req;
+      const { movieId } = req.params;
+      try {
+        const updateMovie = await moviesService.updateMovie({ movieId, movie });
+        res.status(200).json({
+          data: updateMovie,
+          message: 'Movie updated',
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
-    router.delete("/:movieId", async (req, res, next) => {
-        const { movieId } = req.params;
-        try {
-           const deletedMovie = await moviesService.deleteMovie({ movieId });
-           res.status(200).json({
-               data: deletedMovie,
-               message: 'Movie deleted'
-           });
-        } catch(err) {
-            next(err)
-        }
-    });
+  router.delete(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    async (req, res, next) => {
+      const { movieId } = req.params;
+      try {
+        const deletedMovie = await moviesService.deleteMovie({ movieId });
+        res.status(200).json({
+          data: deletedMovie,
+          message: 'Movie deleted',
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+}
 
-
-} 
-
-module.exports = moviesApi
+module.exports = moviesApi;
--- utils/schemas/movies.js
+++ utils/schemas/movies.js
@@ -6,36 +6,34 @@ const movieYearSchema = joi.number().min(1888).max(2077);
 const movieCoverSchema = joi.string().uri();
 const movieDescriptionSchema = joi.string().max(300);
 const movieDurationSchema = joi.number().min(1).max(300);
-const movieContentRattingSchema = joi.string().max(5);
+const moviecontentRatingSchema = joi.string().max(5);
 const movieSourceSchema = joi.string().uri();
-const movieTagSchema = joi.array().items(joi.string().max(50))
+const movieTagSchema = joi.array().items(joi.string().max(50));
 
-
-const createMovie = {
-    title: movieTitleSchema.required(),
-    year: movieYearSchema.required(),
-    cover: movieCoverSchema.required(),
-    description: movieDescriptionSchema.required(),
-    duration: movieDurationSchema.required(),
-    contentRatting: movieContentRattingSchema.required(),
-    source: movieSourceSchema.required(),
-    tags: movieTagSchema
+const createMovieSchema = {
+  title: movieTitleSchema.required(),
+  year: movieYearSchema.required(),
+  cover: movieCoverSchema.required(),
+  description: movieDescriptionSchema.required(),
+  duration: movieDurationSchema.required(),
+  contentRating: moviecontentRatingSchema.required(),
+  source: movieSourceSchema.required(),
+  tags: movieTagSchema,
 };
 
-const updateMovie = {
-    title: movieTitleSchema,
-    year: movieYearSchema,
-    cover: movieCoverSchema,
-    description: movieDescriptionSchema,
-    duration: movieDurationSchema,
-    contentRatting: movieContentRattingSchema,
-    source: movieSourceSchema,
-    tags: movieTagSchema
+const updateMovieSchema = {
+  title: movieTitleSchema,
+  year: movieYearSchema,
+  cover: movieCoverSchema,
+  description: movieDescriptionSchema,
+  duration: movieDurationSchema,
+  contentRating: moviecontentRatingSchema,
+  source: movieSourceSchema,
+  tags: movieTagSchema,
 };
-
 
 module.exports = {
-    movieIdSchema, 
-    createMovie,
-    updateMovie
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema
 };
