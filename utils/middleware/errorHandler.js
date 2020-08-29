908d1aac20a99ae08ae238c3d7f5c5a6e3546439
Andres <andres.rivas@mercadolibre.com>
2020-08-29 08:54:42 -0300

    Added middleware error

--- utils/middleware/errorHandler.js
+++ utils/middleware/errorHandler.js
@@ -1,3 +1,4 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function logErrors(err, req, res, next) {
@@ -5,14 +6,23 @@ function logErrors(err, req, res, next) {
   next(err);
 }
 
+function wrapError(err, req, res, next) {
+  if (!err.isBoom ) {
+    next(boom.badImplementation(err));
+  }
+  next(err);
+}
+
 function errorHandler(err, req, res, next) { //eslint-disable-line
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+const { output: { statusCode, payload } } = err;
+
+  res.status(statusCode || 500);
+  res.json(withErrorStack(payload, err.stack));
 }
 
 function withErrorStack(err, stack) {
   if (config.dev) {
-    return { err, stack };
+    return { ...err, stack };
   }
 
   return err;
@@ -20,5 +30,6 @@ function withErrorStack(err, stack) {
 
 module.exports = { 
     logErrors,
+    wrapError,
     errorHandler
  };
